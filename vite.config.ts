502215cbb628c44f4d715622d64d9b086689931a
oceantrees <107382067+oceantrees@users.noreply.github.com>
2026-02-01 23:49:50 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Fund-realtime/', // 这里换成你的仓库名，比如 '/fundmatrix/'
+  base: '/Fund-Helper/', // 这里换成你的仓库名，比如 '/fundmatrix/'
 })
+
